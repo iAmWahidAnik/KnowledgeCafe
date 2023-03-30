de2647ced36677bb3e545e1748c881dae1d89e23
wahidanik <wahid.anik@outlook.com>
2023-03-30 22:22:36 +0600

    project initialized with tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1 className="text-6xl text-red-600 font-bold">
+      Hello world!
+    </h1>
+    </div>
+  )
+}
+
+export default App
