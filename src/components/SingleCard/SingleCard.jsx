b5223e5de3730751e911ec6499c1c2b7de8ed38e
wahidanik <wahid.anik@outlook.com>
2023-03-31 10:58:52 +0600

    each product added to ui

--- src/components/SingleCard/SingleCard.jsx
+++ src/components/SingleCard/SingleCard.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import './SingleCard.css'
 
-const SingleCard = ({blog}) => {
-    const {cover, authorImage, author, published, readTime, title} = blog;
+const SingleCard = ({ blog }) => {
+    const { cover, authorImage, author, published, readTime, title } = blog;
     return (
         <div>
-            <img className='image' src={cover} alt="" />
+            <img className='cover-image' src={cover} alt="" />
+            <div className='flex'>
+                <div className='flex'>
+                <img className='author-image' src={authorImage} alt="" />
+                <div>
+                    <h4>{author}</h4>
+                    <p>{published}</p>
+                </div>
+                </div>
+                <div>
+                    <p>{readTime} Min Read</p>
+                    <button></button>
+                </div>
+            </div>
+            <h1>{title}</h1>
         </div>
     );
 };
